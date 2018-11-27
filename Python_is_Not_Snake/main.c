e40f5c473b30fb32da4047c4a9c0c601cb7b9d95
MW <mwzdouks@users.noreply.github.com>
2018-11-28 02:28:23 +0800

    Show & Hit Apple Done

--- Python_is_Not_Snake/main.c
+++ Python_is_Not_Snake/main.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<windows.h>
+#include<time.h>
 #define BLACK_GREEN 0x0a															//黑底绿字
 #define BLACK_YELLOW 0x0e															//黑底黄字
-#define BLACK_RED																	//黑底红字
-int snake[10][2], apple[1][2],len,flag=1,j,score=0;
+#define BLACK_RED 0x0c																//黑底红字
+int snake[10][2] = { { 11,11 } ,{ 11, 10 }, { 11, 9 }, { -1, -1} ,{ -1, -1} ,{ -1, -1}, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+int apple_x, apple_y, len, flag = 1, j, score = 0,dir;
 /*初始化参数
 snake储存蛇的x,y坐标,apple储存苹果的x,y坐标,len蛇长
 (列0储存x,列1储存y)*/
@@ -72,65 +74,139 @@ void showstate()
 	gotoxy(24, 8);
 	printf("SCORE:%d", score);
 }
+
+/*显示苹果*/
+void prt_apple()
+{
+	int i, flag = 0;
+	do
+	{
+		flag = 0;
+		apple_x = (int)(20.0*rand() / RAND_MAX + 1.0);									//通过随机数为苹果坐标赋值(0<=rand()/RAND_MAX<=1)
+		apple_y = (int)(20.0*rand() / RAND_MAX + 1.0);
+		for (i = 0; i < 10; i++)														//检查生产苹果是否与蛇重合
+		{
+			if (snake[i][0] == apple_x && snake[i][1] == apple_y)
+			{
+				flag = 1;
+			}
+		}
+
+	} while (flag);
+	gotoxy(apple_x, apple_y);														//显示苹果
+	color(BLACK_RED);
+	printf("●");
+}
 /*初始化参数*/
 void init()
 {
 	int i;
-	system("cls");
-	gotoxy(0, 0);
-	for (i = 0; i < 22; i++)
+	srand((int)time(0));														 //通过系统时间获取随机数种子
+	color(0X00);																 //颜色设定为黑底黑字
+	system("cls");																 //清屏															
+	gotoxy(0, 0);																 //重置坐标
+	for (i = 0; i < 22; i++)													 //输出地图
 	{
-		if (0 < i || i < 22)
+		if (0 < i && i < 22)
 		{	
 			color(BLACK_GREEN);
-			printf("■");
+			printf("□");
 			color(BLACK_YELLOW);
 			printf("■■■■■■■■■■■■■■■■■■■■■■");
 			color(BLACK_GREEN);
-			printf("■\n");
+			printf("□\n");
 		}
 		else
 		{
-			color(BLACK_YELLOW);
-			printf("■■■■■■■■■■■■■■■■■■■■■■■■\n");
+			color(BLACK_GREEN);
+			printf("□□□□□□□□□□□□□□□□□□□□□□□\n");
 		}
 	}
-	gotoxy(0, 0);
-	color(BLACK_GREEN);
-	printf("■■■■■■■■■■■■■■■■■■■■■■■■\n");
-	gotoxy(0, 22);
-	color(BLACK_GREEN);
-	printf("■■■■■■■■■■■■■■■■■■■■■■■■\n");
-	color(BLACK_YELLOW);
+	color(BLACK_YELLOW);															//输出信息
 	gotoxy(24, 0);
 	printf("GROUP NUMBER:     ");
 	gotoxy(24,1);
 	printf("ID:     NAME:S.W.");
 	gotoxy(24, 2);
 	printf("TERRITORY:WHOLE");
-	showstate();
+	showstate();																	//输出动态信息
+	prt_apple();
 }
 
-/*显示地图*/
-void prt_map()
-{
-
-}
-
-/*显示苹果*/
-void prt_apple()
-{
-
-}
 /*控制蛇*/
 void ctrl_snake()
 {
-
+	int i;
+	gotoxy(snake[0][0], snake[0][1]);													 //输出蛇头
+	printf("¤");
+	for (i = 1; i < 10; i++)															 //输出蛇身
+	{
+		if (snake[i][0] > 0)
+		{
+			gotoxy(snake[i][0], snake[i][1]);
+			printf("★");
+		}
+		else																			 //填补地图	空位
+		{
+			color(BLACK_YELLOW);
+			gotoxy(snake[i][0], snake[i][1]);
+			printf("■");
+		}
+		Sleep(350);
+		if (_kbhit())																	 //按键激活
+		{
+			switch (getchar())															 //获得按键
+			{
+			case 'w':
+			case 'W':
+				snake[0][1]++;
+				dir = 0;
+				break;
+			case 's':
+			case 'S':
+				snake[0][1]--;
+				dir = 1;
+				break;
+			case 'a':
+			case 'A':
+				snake[0][0]--;
+				dir = 2;
+				break;
+			case 'd':
+			case 'D':
+				snake[0][0]++;
+				dir = 3;
+				break;
+			default:																	  //保存按键方向									
+				switch (dir)
+				{
+				case 0:
+					snake[0][1]++;
+					break;
+				case 1:
+					snake[0][1]--;
+					break;
+				case 2:
+					snake[0][0]--;
+					break;
+				case 3:
+					snake[0][0]++;
+					break;
+					break;
+				}
+			}
+		}
+		for (i = 1; i < 10; i++)														//蛇身移动(讲上一个蛇身的坐标移至下一个蛇身)
+		{
+			snake[i][0] = snake[i + 1][0];
+			snake[i][1] = snake[i + 1][1];
+		}
+	}
 }
 /*蛇吃到苹果*/
 void eat_apple()
 {
-
+	prt_apple();
 }
 
 /*蛇撞墙*/
@@ -168,18 +244,17 @@ int main()
 			default:
 				return 0;
 			}
-		init();												//初始化参数
-		prt_map();											//显示地图
+		init();	
+		//初始化参数
 		while (1)											//地图关卡循环(屏幕刷新循环)
 		{
 			ctrl_snake();									//控制蛇
-			prt_apple();									//显示苹果
 			if (snake[0][0] == 0 || snake[0][0] == 22 || snake[0][1] == 0 || snake[0][1] == 22)		//判断是否撞墙(蛇头坐标是否与墙重合)
 			{
 				wall();										//显示撞墙后效果
 				break;										//退出屏幕刷新循环
 			}
-			if (snake[0][0] == apple[0] && snake[0][1] == apple[1])
+			if (snake[0][0] == apple_x && snake[0][1] == apple_y)
 			{
 				eat_apple();
 			}
@@ -191,7 +266,7 @@ int main()
 		}
 		if (continue_yn())
 		{
-			break;											//退出游戏循环
+			return 0;											//退出游戏循环
 		}
 	}
 	return 0;
